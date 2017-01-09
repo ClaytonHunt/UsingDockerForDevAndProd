4f847248699aedc50dc2259f99381bec09d5ca74
Clayton Hunt <ClaytonHunt@outlook.com>
2017-01-09 11:51:08 -0500

    Fixed an issue with the volume not mounting properly

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 docker build -f ./src/Dockerfile --tag interactive-docker .
 
-docker run --rm --name interactive-docker -it -v ./src:/app -t interactive-docker
+docker run --rm --name interactive-docker -it -v %CD%\src:/app -t interactive-docker
