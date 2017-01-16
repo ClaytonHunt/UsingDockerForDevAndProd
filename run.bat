4546a395d39092def1d232d29dd691eef9a07ff3
Clayton Hunt <claytonhunt_104@hotmail.com>
2017-01-16 17:38:20 -0500

    Added compose file and configured app to run in watch mode

--- run.bat
+++ run.bat
@@ -2,8 +2,8 @@ cd ./src
 
 docker pull microsoft/aspnetcore-build@sha256:b60565f83a39984f634d61b6a0e21faf5650b19fb1ffc280fc6e47fdeb0290d9
 
-docker build --tag dotnet-development .
-
-docker run --rm --name dotnet-development -it -p 3000:80 -t dotnet-development
+docker build --rm --tag dotnet-development .
 
 cd ..
+
+docker-compose -f docker-compose.dev.debug.yml up
