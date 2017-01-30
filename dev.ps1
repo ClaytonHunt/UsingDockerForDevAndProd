3b3d3138aca44b169469dc339d2ac5782825dc7e
Clayton Hunt <claytonhunt_104@hotmail.com>
2017-01-30 10:51:42 -0500

    had to switch version of dotnet watch back to preview4

--- dev.ps1
+++ dev.ps1
@@ -1,9 +1,11 @@
 cd ./src
 
-docker pull microsoft/aspnetcore-build@sha256:b60565f83a39984f634d61b6a0e21faf5650b19fb1ffc280fc6e47fdeb0290d9
+docker pull microsoft/aspnetcore-build@sha256:80fe1d5a66ae4cf22d71b9e1003db16fde3ba5f9665589ff19b589078d76fc66
 
 docker build --rm --tag dotnet-development .
 
 cd ..
+
+start http://localhost:3000/
 
 docker-compose -f docker-compose.dev.yml up
