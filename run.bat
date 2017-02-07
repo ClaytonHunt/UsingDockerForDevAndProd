50bee9de29911683b30068bf18a8d9383dc69cc9
Clayton Hunt <claytonhunt@outlook.com>
2017-02-07 04:47:21 -0500

    moved application into a directory

--- run.bat
+++ run.bat
@@ -1,6 +1,4 @@
-cd ./src
-
-docker pull microsoft/aspnetcore-build@sha256:b60565f83a39984f634d61b6a0e21faf5650b19fb1ffc280fc6e47fdeb0290d9
+cd ./src/app.web
 
 docker build --tag dotnet-development .
 
