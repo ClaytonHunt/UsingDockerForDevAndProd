d48f07d2b314699f9abcb2120c1d2ffd5ee2c0a8
Clayton Hunt <claytonhunt@outlook.com>
2018-03-28 05:57:37 -0400

    new project from template

---
+++ application.web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace application.web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
