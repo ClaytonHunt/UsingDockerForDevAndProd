403c9e78ca31f88c6530d0e4744c141f26e01124
Clayton Hunt <claytonhunt@outlook.com>
2018-03-29 16:46:19 -0400

    its mostly working

--- application.web/Pages/Index.cshtml.cs
+++ application.web/Pages/Index.cshtml.cs
@@ -4,9 +4,14 @@ namespace application.web.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public int? AddValue {get; private set;}
+        public void OnGet(int? a, int? b)
         {
-
+            if(a == null && b == null) {
+                AddValue = null;
+            } else {
+                AddValue = (a ?? 0) + (b ?? 0);
+            }
         }
     }
 }
